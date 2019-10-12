49b2121f05bce5e9cfb54a44c95b72a9ca62b75f
Andrew Gregory <andrew.gregory.8@gmail.com>
2019-10-11 20:31:42 -0700

    update systemv functions

--- c/systemv.c
+++ c/systemv.c
@@ -1,78 +1,137 @@
-#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 extern char **environ;
 
-int systemvp(const char *file, char *const argv[]) {
-    int *err = mmap(NULL, sizeof(*err), PROT_READ | PROT_WRITE,
-            MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    int pid = fork();
+/* system()/exec() hybrid functions allowing exec()-style direct execution
+ * of a command with the simplicity of system()
+ * - not thread-safe
+ * - errno may be set by fork(), pipe(), or execve()/execvp()
+ */
 
-    if(pid == -1) {
-        /* error */
-        munmap(err, sizeof(*err));
+int systemvp(const char *file, char *const argv[]) {
+    int pid, err = 0, ret = -1, err_fd[2];
+    sigset_t oldblock;
+    struct sigaction sa_ign = { .sa_handler = SIG_IGN }, oldint, oldquit;
+
+    if(pipe(err_fd) != 0) {
         return -1;
-    } else if(pid == 0) {
-        /* child */
-        *err = 0;
-        execvp(file, argv);
-        *err = errno;
-        munmap(err, sizeof(*err));
-        _Exit(1);
-    } else {
-        /* parent */
-        int status;
-        while(waitpid(pid, &status, 0) == -1) {
-            if(errno != EINTR) {
-                munmap(err, sizeof(*err));
-                return -1;
-            }
-        }
-        if(*err != 0) {
-            errno = *err;
-            status = -1;
-        }
-        munmap(err, sizeof(*err));
-        return status;
     }
+
+    sigaction(SIGINT, &sa_ign, &oldint);
+    sigaction(SIGQUIT, &sa_ign, &oldquit);
+    sigaddset(&sa_ign.sa_mask, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &sa_ign.sa_mask, &oldblock);
+
+    pid = fork();
+
+    /* child */
+    if(pid == 0) {
+        close(err_fd[0]);
+        fcntl(err_fd[1], F_SETFD, FD_CLOEXEC);
+
+        /* restore signal handling for the child to inherit */
+        sigaction(SIGINT, &oldint, NULL);
+        sigaction(SIGQUIT, &oldquit, NULL);
+        sigprocmask(SIG_SETMASK, &oldblock, NULL);
+
+        execvp(file, argv);
+
+        /* execvp failed, pass the error back to the parent */
+        while(write(err_fd[1], &errno, sizeof(errno)) == -1 && errno == EINTR);
+        _Exit(127);
+    }
+
+    /* parent */
+    close(err_fd[1]);
+
+    if(pid != -1)  {
+        int wret;
+        while((wret = waitpid(pid, &ret, 0)) == -1 && errno == EINTR);
+        if(wret > 0) {
+            while(read(err_fd[0], &err, sizeof(err)) == -1 && errno == EINTR);
+        }
+    } else {
+        /* fork failed, make sure errno is preserved after cleanup */
+        err = errno;
+    }
+
+    close(err_fd[0]);
+
+    sigaction(SIGINT, &oldint, NULL);
+    sigaction(SIGQUIT, &oldquit, NULL);
+    sigprocmask(SIG_SETMASK, &oldblock, NULL);
+
+    if(err) {
+        errno = err;
+        ret = -1;
+    }
+
+    return ret;
 }
 
 int systemve(const char *path, char *const argv[], char *const envp[]) {
-    int *err = mmap(NULL, sizeof(*err), PROT_READ | PROT_WRITE,
-            MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    int pid = fork();
+    int pid, err = 0, ret = -1, err_fd[2];
+    sigset_t oldblock;
+    struct sigaction sa_ign = { .sa_handler = SIG_IGN }, oldint, oldquit;
 
-    if(pid == -1) {
-        /* error */
-        munmap(err, sizeof(*err));
+    if(pipe(err_fd) != 0) {
         return -1;
-    } else if(pid == 0) {
-        /* child */
-        *err = 0;
-        execve(path, argv, envp);
-        *err = errno;
-        munmap(err, sizeof(*err));
-        _Exit(1);
-    } else {
-        /* parent */
-        int status;
-        while(waitpid(pid, &status, 0) == -1) {
-            if(errno != EINTR) {
-                munmap(err, sizeof(*err));
-                return -1;
-            }
-        }
-        if(*err != 0) {
-            errno = *err;
-            status = -1;
-        }
-        munmap(err, sizeof(*err));
-        return status;
     }
+
+    sigaction(SIGINT, &sa_ign, &oldint);
+    sigaction(SIGQUIT, &sa_ign, &oldquit);
+    sigaddset(&sa_ign.sa_mask, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &sa_ign.sa_mask, &oldblock);
+
+    pid = fork();
+
+    /* child */
+    if(pid == 0) {
+        close(err_fd[0]);
+        fcntl(err_fd[1], F_SETFL, FD_CLOEXEC);
+
+        sigaction(SIGINT, &oldint, NULL);
+        sigaction(SIGQUIT, &oldquit, NULL);
+        sigprocmask(SIG_SETMASK, &oldblock, NULL);
+
+        execve(path, argv, envp);
+
+        write(err_fd[1], &errno, sizeof(errno));
+        _Exit(127);
+    }
+
+    /* parent */
+    close(err_fd[1]);
+
+    if(pid != -1)  {
+        int wret;
+        while((wret = waitpid(pid, &ret, 0)) == -1 && errno == EINTR);
+        if(wret > 0 && WEXITSTATUS(ret) == 127) {
+            read(err_fd[0], &err, sizeof(err));
+        }
+    } else {
+        /* fork failed, make sure errno is preserved after cleanup */
+        err = errno;
+    }
+
+    close(err_fd[0]);
+
+    sigaction(SIGINT, &oldint, NULL);
+    sigaction(SIGQUIT, &oldquit, NULL);
+    sigprocmask(SIG_SETMASK, &oldblock, NULL);
+
+    if(err) {
+        errno = err;
+        ret = -1;
+    }
+
+    return ret;
 }
 
 int systemv(const char *path, char *const argv[]) {
@@ -130,7 +189,7 @@ int systemle(const char *path, ...) {
     }
 
     /* copy args */
-    count = 0;
+    i = 0;
     va_start(ap, path);
     while(i < count) {
         argv[i++] = va_arg(ap, char*);
--- c/t/10-systemv.c
+++ c/t/10-systemv.c
@@ -8,14 +8,17 @@ int main(void) {
     tap_plan(9);
 
     tap_is_int( systemv(cmd[0], cmd), 0, "exit 0" );
+    errno = 0;
     tap_is_int( systemv("non-existent", cmd), -1, "non-existent file - return" );
     tap_is_int( errno, ENOENT, "non-existent file - errno" );
 
     tap_is_int( systemvp("sh", cmd), 0, "exit 0" );
+    errno = 0;
     tap_is_int( systemvp("non-existent", cmd), -1, "non-existent file - return" );
     tap_is_int( errno, ENOENT, "non-existent file - errno" );
 
     tap_is_int( systeml("/bin/sh", "/bin/sh", "-c", "exit 0", NULL), 0, "exit 0" );
+    errno = 0;
     tap_is_int( systeml("non-existent", "foo", NULL), -1, "non-existent file - return" );
     tap_is_int( errno, ENOENT, "non-existent file - errno" );
 
